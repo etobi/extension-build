be8debb50372d16d178dcb335c37d54c0bb9fd35
Tobias Liebig <work@etobi.de>
2016-06-22 10:22:09 +0200

    [TASK] upload to TER from travis

---
+++ scripts/travis/uploadToTER.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ev
+
+echo "Tag: $TRAVIS_TAG";
+
+if [ -n "$TRAVIS_TAG" ] && [ -n "$TYPO3_ORG_USERNAME" ] && [ -n "$TYPO3_ORG_PASSWORD" ]; then
+	echo "Preparing upload of release ${TRAVIS_TAG} to TER"
+	curl -sSL https://raw.githubusercontent.com/alrra/travis-after-all/1.4.4/lib/travis-after-all.js | node
+	if [ $? -eq 0 ]; then
+  		# Cleanup before we upload
+  		git reset --hard HEAD && git clean -fx
+  		TAG_MESSAGE=`git tag -n10 -l $TRAVIS_TAG | sed "s/^[0-9.]*[ ]*//g"`
+  		echo "Uploading release ${TRAVIS_TAG} to TER (${TAG_MESSAGE})"
+  		.Build/bin/upload . "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
+  		if [ $? -eq 0 ]; then
+    			exit 0;
+  		else
+    			echo "Error while uploading to TER"
+    			exit 1;
+  		fi;
+	else
+  		echo "Error while waiting for other jobs"
+  		exit 2;
+	fi;
+fi;
+
